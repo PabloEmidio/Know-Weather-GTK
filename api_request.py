32b5d7c76149fc6efefea30a2aeeab8adad3f1f1
utrape <pablosud025@gmail.com>
2021-05-06 14:39:29 -0300

    new interface's code complete

--- api_request.py
+++ api_request.py
@@ -17,10 +17,18 @@ class Weather:
         url = cls.BASE_URL + cls.API_METHOD + '?' + cls.API_KEY + '&q=' + city + '&days=3'
         weather_info = cls.search(url)
         os.system('wget ' + weather_info['current']['condition']['icon'][2:] + ' -O ./images/weather_icon.png')
+        
+        # download hours icon
         os.system('wget ' + weather_info['forecast']['forecastday'][0]['hour'][current_hour]['condition']['icon'][2:] + f' -O ./images/hour_icon/1.png')
         os.system('wget ' + weather_info['forecast']['forecastday'][0]['hour'][current_hour+1]['condition']['icon'][2:] + f' -O ./images/hour_icon/2.png')
         os.system('wget ' + weather_info['forecast']['forecastday'][0]['hour'][current_hour+2]['condition']['icon'][2:] + f' -O ./images/hour_icon/3.png')
         os.system('wget ' + weather_info['forecast']['forecastday'][0]['hour'][current_hour+3]['condition']['icon'][2:] + f' -O ./images/hour_icon/4.png')
         os.system('wget ' + weather_info['forecast']['forecastday'][0]['hour'][current_hour+4]['condition']['icon'][2:] + f' -O ./images/hour_icon/5.png')
+    
+        # download days icon
+        os.system('wget ' + weather_info['forecast']['forecastday'][1]['hour'][current_hour]['condition']['icon'][2:] + f' -O ./images/days_icon/1.png')
+        os.system('wget ' + weather_info['forecast']['forecastday'][2]['hour'][current_hour]['condition']['icon'][2:] + f' -O ./images/days_icon/2.png')        
+        
         return weather_info
+    
 
